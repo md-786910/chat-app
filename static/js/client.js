1a676854437ba5505634e5270e7c2cd5f75717c4
md ashif reza <mdashifreza111@gmail.com>
2021-06-14 14:31:01 +0530

    my first commit

--- static/js/client.js
+++ static/js/client.js
@@ -1,5 +1,5 @@
 
-const socket = io("http://wechating-app.herokuapp.com/");
+const socket = io("http://localhost:5000/");
 const chatform = document.getElementById("chat-form");
 const right = document.querySelector(".right");
 
